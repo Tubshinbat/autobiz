524f4c296e64100608a44de39efad769ba4ed1a2
tuvshinbat <tuvshinbat596@gmail.com>
2022-05-23 06:11:12 +0800

    order page

--- pages/purchase/[id].js
+++ pages/purchase/[id].js
@@ -267,8 +267,8 @@ export default ({ product, user }) => {
 export const getServerSideProps = async function ({ req, res, params }) {
   let user = {};
 
-  let token = req.cookies.autobiztoken;
-  if (token) user = await getUser(token);
+  // let token = req.cookies.autobiztoken;
+  // if (token) user = await getUser(token);
 
   const { id } = params;
   console.log(id);
